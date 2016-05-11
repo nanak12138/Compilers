342f9e39c56fcc725d05c2d4e3cb74698c714554
李一帆nanak <ldh696@163.com>
2016-05-11 08:54:17 +0800

    Initial Commit

---
+++ Compilers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Compilers
+//
+//  Created by 李一帆nanak on 16/5/11.
+//  Copyright © 2016年 李一帆nanak. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
